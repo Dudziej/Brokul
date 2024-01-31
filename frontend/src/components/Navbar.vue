c0a37d2c9ef16aba01c5e57321d5df3b917398e4
Dudziej <dudzik32@gmail.com>
2024-01-31 17:18:16 +0100

    minor navbar edit

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,21 +1,37 @@
 <template>
   <v-app-bar app color="grey darken-3" dark>
-    <v-btn color="primary" class="nav-button" to="/">
-      Diety od Brokuła
+    <v-btn color="secondary" class="nav-button" to="/">
+      <v-icon dark>mdi-home</v-icon>
     </v-btn>
 
     <v-spacer></v-spacer>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+          <v-icon dark class="nav-button" v-bind="attrs" v-on="on">mdi-menu</v-icon>
+      </template>
 
-    <v-btn color="primary" class="nav-button" to="/products">Produkty</v-btn>
-    <v-btn color="primary" class="nav-button" to="/sales-report">Raport sprzedaży</v-btn>
-    <v-btn color="primary" class="nav-button" to="/order-form">Formularz zamówień</v-btn>
+      <v-list>
+        <v-list-item to="/products">
+          <v-list-item-title>Produkty</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/sales-report">
+          <v-list-item-title>Raport sprzedaży</v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/order-form">
+          <v-list-item-title>Formularz zamówień</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
+
+
 export default {
   name: 'NavbarComponent'
 }
+
 </script>
 
 <style>
