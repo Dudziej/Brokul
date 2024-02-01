425e8f76afd984da2fb51bec22e022525f7eb947
Dudziej <dudzik32@gmail.com>
2024-02-01 18:01:19 +0100

    Added a PieChart component that renders a chart on SalesReport view with chart.js package

---
+++ frontend/src/components/PieChart.vue
@@ -0,0 +1,20 @@
+  <script>
+  import { Pie } from 'vue-chartjs';
+  
+  export default {
+    extends: Pie,
+    props: {
+      chartData: {
+        type: Object,
+        required: true
+      },
+      chartOptions: {
+        type: Object,
+        default: () => {}
+      }
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.chartOptions)
+    }
+  }
+  </script>
--- frontend/src/views/SalesReport.vue
+++ frontend/src/views/SalesReport.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="12" sm="8" md="6">
+      <v-col cols="12" md="8">
         <v-card class="pa-5" elevation="2">
           <v-card-title class="text-h5">
             Generowanie Raportu Sprzedaży
@@ -9,72 +9,68 @@
           <v-card-text>
             <v-row>
               <v-col cols="12" md="4">
-                <v-menu
-                  ref="startMenu"
-                  v-model="menu1"
-                  :close-on-content-click="false"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-                >
+                <v-menu ref="startMenu"
+                        v-model="menu1"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px">
                   <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="dateRange.start"
-                      label="Data początkowa"
-                      prepend-icon="mdi-calendar"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
+                    <v-text-field v-model="dateRange.start"
+                                  label="Data początkowa"
+                                  prepend-icon="mdi-calendar"
+                                  readonly
+                                  v-bind="attrs"
+                                  v-on="on">
+                    </v-text-field>
                   </template>
-                  <v-date-picker v-model="dateRange.start" @input="menu1 = false"></v-date-picker>
+                  <v-date-picker v-model="dateRange.start" 
+                                 @input="menu1 = false">
+                  </v-date-picker>
                 </v-menu>
               </v-col>
               <v-col cols="12" md="4">
-                <v-menu
-                  ref="endMenu"
-                  v-model="menu2"
-                  :close-on-content-click="false"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-                >
+                <v-menu ref="endMenu"
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px">
                   <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="dateRange.end"
-                      label="Data końcowa"
-                      prepend-icon="mdi-calendar"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
+                    <v-text-field v-model="dateRange.end"
+                                  label="Data końcowa"
+                                  prepend-icon="mdi-calendar"
+                                  readonly
+                                  v-bind="attrs"
+                                  v-on="on">
+                                </v-text-field>
                   </template>
-                  <v-date-picker v-model="dateRange.end" @input="menu2 = false"></v-date-picker>
+                  <v-date-picker v-model="dateRange.end" 
+                                 @input="menu2 = false">
+                  </v-date-picker>
                 </v-menu>
               </v-col>
               <v-col cols="12" md="4">
-                <v-select
-                  :items="customers"
-                  item-text="name"
-                  item-value="_id"
-                  label="Wybierz klienta"
-                  return-object
-                  v-model="selectedCustomer"
-                ></v-select>
+                <v-select :items="customers"
+                          item-text="name"
+                          item-value="_id"
+                          label="Wybierz klienta"
+                          return-object
+                          v-model="selectedCustomer">
+                </v-select>
               </v-col>
             </v-row>
             <v-btn @click="generateReport">Generuj Raport</v-btn>
           </v-card-text>
           <v-card-text>
-            <v-data-table
-              :headers="reportHeaders"
-              :items="salesData"
-              :items-per-page="5"
-              class="elevation-1">
+            <v-data-table :headers="reportHeaders"
+                          :items="salesData"
+                          :items-per-page="5"
+                          class="elevation-1">
                 <template v-slot:[`item.totalSales`]="{ item }">
-                  {{ item.totalSales | currency }}
+                    {{ item.totalSales | currency }}
                 </template>
             </v-data-table>
             <v-divider></v-divider>
@@ -85,15 +81,30 @@
           </v-card-text>
         </v-card>
       </v-col>
+      <v-col cols="12" md="4">
+        <v-card class="pa-5" elevation="2">
+          <v-card-title class="text-h5">Udział Sprzedaży Produktów</v-card-title>
+          <v-card-text>
+            <pie-chart :chart-data="chartData"
+                       :options="chartOptions"
+                       v-if="chartData && chartData.datasets && chartData.datasets.length > 0"
+                       :key="chartDataKey"/>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import PieChart from '../components/PieChart.vue';
 
 export default {
   name: 'SalesReport',
+  components: {
+    PieChart,
+  },
   data() {
     return {
       menu1: false,
@@ -110,6 +121,21 @@ export default {
         { text: 'Całkowita Wartość Sprzedaży', value: 'totalSales' },
         { text: 'Nazwa Klienta', value: 'customerNames', sortable: false },
       ],
+      chartDataKey: 0,
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Sprzedaż',
+            backgroundColor: [],
+            data: []
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
     };
   },
   computed: {
@@ -137,19 +163,51 @@ export default {
     ...mapActions('sales', ['fetchSalesData']),
     ...mapActions('customers', ['fetchCustomers']),
 
-    generateReport() {
+    async generateReport() {
       const startDate = this.dateRange.start ? this.formatDate(this.dateRange.start) : undefined;
       const endDate = this.dateRange.end ? this.formatDate(this.dateRange.end) : undefined;
       const customerId = this.selectedCustomer ? this.selectedCustomer._id : undefined;
-      this.fetchSalesData({ startDate, endDate, customerId });
+
+      try {
+        await this.fetchSalesData({ startDate, endDate, customerId });
+        this.generateChartData();
+      } catch (error) {
+        console.error('Error generating report:', error);
+      }
+    },
+
+    generateChartData() {
+      const productNames = this.salesData.map(data => data.name);
+      const productSales = this.salesData.map(data => data.totalSales);
+
+      this.chartData = {
+        labels: productNames,
+        datasets: [
+          {
+            label: 'Sprzedaż',
+            backgroundColor: this.generateColors(productNames.length),
+            data: productSales
+          }
+        ]
+      };
+      this.chartDataKey++;
+    },
+
+    generateColors(count) {
+      const colors = [];
+      for (let i = 0; i < count; i++) {
+        const color = `hsl(${360 * Math.random()}, 50%, 50%)`;
+        colors.push(color);
+      }
+      return colors;
     },
 
     formatDate(date) {
-        if (date) {
-            let dateObj = date instanceof Date ? date : new Date(date);
-            return dateObj.toISOString().split('T')[0];
-        }
-        return null;
+      if (date) {
+        let dateObj = date instanceof Date ? date : new Date(date);
+        return dateObj.toISOString().split('T')[0];
+      }
+      return null;
     },
   },
   mounted() {
